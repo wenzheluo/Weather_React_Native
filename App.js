ab4b522051c44c08ed6ec48bf0e818102c8df718
Wenzhe Luo <100318920+manzitlo@users.noreply.github.com>
2023-09-12 15:50:31 -0400

    set App.js and added components inside App.js

--- App.js
+++ App.js
@@ -3,8 +3,58 @@ import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { Home } from "./pages/Home/Home";
 import { ImageBackground } from "react-native";
 import backgroundImg from "./assets/background.jpg";
+import { useEffect, useState } from "react";
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+} from "expo-location";
+import { MeteoAPI } from "./api/meteo";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [coordinates, setCoordinates] = useState();
+  const [weather, setWeather] = useState();
+  const [city, setCity] = useState();
+  const [isFontLoaded] = useFonts({
+    "Alata-Regular": require("./assets/fonts/Alata-Regular.ttf"),
+  });
+
+  console.log(isFontLoaded);
+  useEffect(() => {
+    getUserCoordicates();
+  }, []);
+
+  useEffect(() => {
+    if (coordinates) {
+      fetchWeatherByCoords(coordinates);
+      fetchCityByCoords(coordinates);
+    }
+  }, [coordinates]);
+
+  async function fetchWeatherByCoords(coords) {
+    const weatherResponse = await MeteoAPI.fetchWeatherByCoords(coords);
+    setWeather(weatherResponse);
+  }
+
+  async function fetchCityByCoords(coords) {
+    const cityResponse = await MeteoAPI.fetchCityByCoords(coords);
+    setCity(cityResponse);
+  }
+
+  async function getUserCoordicates() {
+    const status = await requestForegroundPermissionsAsync();
+
+    if (status === "granted") {
+      const location = await getCurrentPositionAsync();
+      setCoordinates({
+        lat: location.coords.latitude,
+        lng: location.coords.longitude,
+      });
+    } else {
+      setCoordinates({ lat: "38.927", lng: "-77.224" });
+    }
+  }
+
   return (
     <ImageBackground
       imageStyle={s.img}
@@ -13,7 +63,7 @@ export default function App() {
     >
       <SafeAreaProvider>
         <SafeAreaView style={s.container}>
-          <Home />
+          {isFontLoaded && weather && <Home city={city} weather={weather} />}
         </SafeAreaView>
       </SafeAreaProvider>
     </ImageBackground>
