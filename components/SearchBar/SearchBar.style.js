d8092167eab556e0ddd94cd2846e6d612da7bf05
Wenzhe Luo <100318920+manzitlo@users.noreply.github.com>
2023-09-12 16:41:51 -0400

    new version for React native app

---
+++ components/SearchBar/SearchBar.style.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+const s = StyleSheet.create({
+  input: {
+    backgroundColor: "white",
+    height: 45,
+    paddingLeft: 20,
+    borderRadius: 35,
+    fontFamily: "Alata-Regular",
+  },
+});
+
+export { s };
