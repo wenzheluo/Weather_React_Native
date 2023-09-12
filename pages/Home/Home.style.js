48c68f9a58768117774903fcb33fc836877e7531
Wenzhe Luo <100318920+manzitlo@users.noreply.github.com>
2023-09-12 17:25:51 -0400

    final version

---
+++ pages/Home/Home.style.js
@@ -0,0 +1,7 @@
+import { StyleSheet } from "react-native";
+
+export const s = StyleSheet.create({
+  meteo_basic: { flex: 2.5 },
+  searchbar_container: { flex: 1.5 },
+  meteo_advanced: { flex: 1.5 },
+});
